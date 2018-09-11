a29d578d30052bbef5ea1a5c6eb6c9234c764c24
dpfrakes <dpfrakes@gmail.com>
2018-09-11 16:24:02 -0400

    Initial commit

---
+++ responsive-grid.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://www.dpfrakes.net/
+ * @since             1.0.0
+ * @package           Responsive_Grid
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Responsive Grid
+ * Plugin URI:        https://github.com/natgeosociety/
+ * Description:       Simple responsive grid layout for Visual Editor.
+ * Version:           1.0.0
+ * Author:            Dan Frakes
+ * Author URI:        https://www.dpfrakes.net/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       responsive-grid
+ * Domain Path:       /languages
+ */
+
